0b7ab9c6a5e04e336277d89d5bacfec49a24a60c
anastojkovicc <ana.stojkovic98@gmail.com>
2021-08-31 17:45:52 +0200

    Obradjen logout

--- fun/connect.php
+++ fun/connect.php
@@ -1,7 +1,7 @@
 <?php
 $mysql_server= "localhost:8080";
 $mysql_user="root";
-$mysql_pass= "";
+$mysql_pass= "newpassword";
 $mysql_db= "bookshelf";
 $mysqli = new mysqli($mysql_server,$mysql_user,$mysql_pass,$mysql_db);
 
---
+++ menu.php
@@ -0,0 +1,29 @@
+<?php
+include "fun/notLoggedIn.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bookshelf</title>
+    <script
+  src="https://code.jquery.com/jquery-3.6.0.js"
+  integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+  crossorigin="anonymous"></script>
+    <script type="text/javascript">
+    function logout(){
+        $.get("fun/logout.php");
+    }
+    </script>
+</head>
+<body>
+<h1>BOOKSHELF</h1>
+<ul>
+    <a href="index.php" onclick="logout()">
+    <li>Log out</li>
+    </a>
+</ul>
+</body>
+</html>
